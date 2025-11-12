417ea53fffeff08a4b8a2eeb6d15c185eea4f628
ToleInventor <168644336+ToleInventor@users.noreply.github.com>
2025-11-12 18:44:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -244,4 +244,4 @@ def unfollow_user(followed_username):
     return jsonify({'status':'ok', 'message':'Unfollowed successfully'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
